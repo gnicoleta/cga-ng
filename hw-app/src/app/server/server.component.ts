449f9415eed82bf1f3ffdff41ce13bebdfbfca23
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-08-07 12:42:02 +0300

    Attribute directives

--- hw-app/src/app/server/server.component.ts
+++ hw-app/src/app/server/server.component.ts
@@ -2,25 +2,41 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`.online {
+        color:white;
+    }`]
 })
 export class ServerComponent {
 
-    public name : string = "";
-    public serverName : string = "Empty";
-    serverCreated : boolean = false;
+    public name: string = "";
+    public serverName: string = "Empty";
+    serverCreated: boolean = false;
+    serverStatus: string = "offline";
 
-    public onUpdateServerName(event : any) {
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+    }
+
+    public onUpdateServerName(event: any) {
         this.name = (<HTMLInputElement>event.target).value;
     }
 
     public isDisabled() {
         return !this.serverName;
-      }
-    
-      public onAddServer() {
+    }
+
+    public onAddServer() {
         this.serverCreated = true;
         //this.serverName = "";
-      }
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+
+    serevrStatusCheck() {
+        return !!(this.serverStatus === 'online');
+    }
 
 }
