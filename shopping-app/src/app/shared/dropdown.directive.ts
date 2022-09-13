acd379e76e6baa241f74833660e9a9ac2283b598
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-13 22:17:30 +0300

    Improve dropdown directive

--- shopping-app/src/app/shared/dropdown.directive.ts
+++ shopping-app/src/app/shared/dropdown.directive.ts
@@ -6,4 +6,10 @@ export class DropdownDirective {
     @HostListener('click') toggleOpen() {
         this.isOpen = !this.isOpen;
     }
+
+    //improvement to close on clcik outside
+    // @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    //     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    // }
+    // constructor(private elRef: ElementRef) { }
 }
