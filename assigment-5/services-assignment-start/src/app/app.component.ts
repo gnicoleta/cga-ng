a680a59e92e85b50ecf212b7d621e82990e83bb6
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-14 18:07:20 +0300

    Assigment 5 - services

---
+++ assigment-5/services-assignment-start/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CounterService } from './services/counter.service';
+import { UsersService } from './services/users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(public usersService : UsersService, public counterService:CounterService){}
+}
---
+++ assigment-5/services-assignment-start/src/app/services/counter.service.ts
@@ -0,0 +1,6 @@
+export class CounterService {
+    public counter : number = 0;
+    public incrementActionCounter() {
+        this.counter++;
+    }
+}
