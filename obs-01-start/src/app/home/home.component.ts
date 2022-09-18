d272320312abef8b0a57f0c821369b51da528b95
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-19 01:30:01 +0300

    Created custom observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs'
+import { interval, Subscription, Observable } from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -10,14 +10,25 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
-  private intervalSubscription: Subscription;
-  
+  private customObservaleSubscription: Subscription;
+
   ngOnDestroy(): void {
-    this.intervalSubscription.unsubscribe()
+    this.customObservaleSubscription.unsubscribe()
   }
 
   ngOnInit() {
-    this.intervalSubscription = interval(1000).subscribe((count) => { })
+    // this.intervalSubscription = interval(1000).subscribe((count) => { })
+    const customObservale = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
+    });
+
+    this.customObservaleSubscription = customObservale.subscribe((datra) => {
+      console.log("###: " + datra);
+    })
   }
 
 }
