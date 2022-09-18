a0eb604ff58a534c6091d9ef5e98228ec6e69a9c
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-19 01:25:09 +0300

    Analyze observables

---
+++ obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  private intervalSubscription: Subscription;
+  
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe()
+  }
+
+  ngOnInit() {
+    this.intervalSubscription = interval(1000).subscribe((count) => { })
+  }
+
+}
