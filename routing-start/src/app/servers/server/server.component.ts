0cf38a2593b4fae3c118a3aa614823cd58f35bfc
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-18 14:33:42 +0300

    Resolving dynamic data with resolve guard

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -17,11 +17,15 @@ export class ServerComponent implements OnInit {
     private router: Router) { }
 
     ngOnInit() {
-      const id =+ this.route.snapshot.params['id'];
-      this.server = this.serversService.getServer(id);
+      // const id =+ this.route.snapshot.params['id'];
+      // this.server = this.serversService.getServer(id);
 
-      this.route.params.subscribe((params : Params) => {
-        this.server = this.serversService.getServer(+params['id'])
+      // this.route.params.subscribe((params : Params) => {
+      //   this.server = this.serversService.getServer(+params['id'])
+      // })
+
+      this.route.data.subscribe((data : Data) => {
+        this.server = data['server']
       })
     }
   
