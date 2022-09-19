6d1712138dc3bf123e19fa17e60f6041757e91b5
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-19 20:38:06 +0300

    Template driven forms (TD forms) and validation/warnings

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm : NgForm; //if you want to acces teh form earlier
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit() {
+  }
+}
