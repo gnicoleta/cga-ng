784c8bfb3fef81b3e25c59546bb29bb796acbb74
gnicoleta <nicoleta.gafitanu@gmail.com>
2022-09-19 21:48:19 +0300

    Assigment 6

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,14 +9,12 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm : NgForm; //if you want to acces teh form earlier
 
-  answer = "";
-
-  genders = ['male','female']
+  subscriptions = ['Basic','Advanced', 'Pro']
 
   user = {
-    username:'',
+    password:'',
     email:'',
-    secretQuestion:'',
+    subscription:'',
     answer :'',
     gender:''
 
@@ -24,35 +22,13 @@ export class AppComponent {
 
   submitted = false;
 
-  defaultQuestion ='pet';
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email:''
-    //   },
-    //   secret:'pet',
-    //   questionAnswer:'',
-    //   gender:'male'
-    // });
-
-    this.signupForm.form.patchValue({
-      userData: {
-        username:suggestedName
-      }
-    })
-
-  }
+  subscriptionDefault ='basic';
 
   onSubmit() {
     this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
+    this.user.password = this.signupForm.value.userData.password;
     this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
+    this.user.subscription = this.signupForm.value.subscription;
 
     this.signupForm.reset(); //will clean everything, every field etc.
   }
